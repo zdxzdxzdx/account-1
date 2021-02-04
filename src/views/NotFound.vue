6bc59845281a34902e3ec0ae877903f18611cc94
zdxzdxzdx <769371987@qq.com>
2021-02-04 19:18:36 +0800

    添加404页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+
+    <div>
+        <div>当前页面不存在，请检查网址是否正确</div>
+        <router-link to="/">返回首页</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'NotFound'
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
