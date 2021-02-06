65327e682ad8e4caa839cf16041a15cda5ab5103
zdxzdxzdx <769371987@qq.com>
2021-02-06 10:23:57 +0800

    添加全局样式和全局变量

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -24,6 +24,7 @@
 </script>
 
 <style scoped lang="scss">
+    @import "~@/assets/style/helper.scss";
     nav {
         display: flex;
         box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
@@ -43,7 +44,7 @@
 
         }
         >.item.selected {
-            color: red;
+            color: $color-highlight;
         }
     }
 
