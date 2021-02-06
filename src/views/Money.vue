c0e3716c5e2d36d0d4137edaeda07f7883162021
zdxzdxzdx <769371987@qq.com>
2021-02-06 20:39:32 +0800

    模块化：把Money.vue分成四个子组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,63 +1,22 @@
 <template>
     <Layout class-prefix="layout">
-        <div class="numberPad">
-            <div class="output">100</div>
-            <div class="buttons ">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>删除</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>清空</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button class="ok">OK</button>
-                <button class="zero">0</button>
-                <button>.</button>
-            </div>
-        </div>
-
-
-        <div>
-            <ul class="types">
-                <li class="selected" >支出</li>
-                <li>收入</li>
-            </ul>
-        </div>
-        <div>
-            <label class="notes">
-                <span class="name"  >备注</span>
-                <input type="text" placeholder="在这里输入备注">
-            </label>
-        </div>
-        <div class="tags">
-            <div class="new">
-                <button>新增标签</button>
-            </div>
-            <ul class="current">
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
-            </ul>
-
-        </div>
+        <NumberPad/>
+        <Types/>
+        <Notes/>
+        <Tags/>
 
     </Layout>
 </template>
 
 
 <script lang="ts">
+  import NumberPad from '@/components/Money/NumberPad.vue';
+  import Types from '@/components/Money/Types.vue';
+  import Notes from '@/components/Money/Notes.vue';
+  import Tags from '@/components/Money/Tags.vue';
   export default {
     name: 'Money',
-
+    components: {Tags, Notes, Types, NumberPad},
   };
 </script>
 <style lang="scss">
@@ -70,113 +29,6 @@
 </style>
 
 <style scoped lang="scss">
-    @import "~@/assets/style/helper.scss";
-    .numberPad{
-        .output {
-            @extend %clearFix;
-            @extend  %innerShadow;
-            font-size: 36px;
-            font-family: Consolas,monospace;
-            padding: 9px 16px;
-            text-align: right;
 
-        }
-        .buttons{
-            @extend %clearFix;
-            > button{
-                width: 25%;
-                height: 64px;
-                float: left;
-                background: #F2F2F2;
-                border: none;
-                &.ok{
-                    height: 64px*2 ;
-                    float: right;
-                }
-                &.zero {
-                    width: 25*2%;
-                }
-                &:nth-child(12){
-                    background: #F0DE38;
-                }
-
-            }
-        }
-
-    }
-
-    .types{
-        background: #F0DE38;
-        display: flex;
-        text-align: center;
-        font-size: 24px;
-        > li {
-            width: 50%;
-            height: 64px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: relative;
-            &.selected::after {
-                content: '';
-                position: absolute;
-                bottom: 0;
-                left: 0;
-                width: 100%;
-                height: 4px;
-                background: #333;
-
-            }
-        }
-    }
-    .notes{
-        font-size: 14px;
-        background: #f5f5f5;
-        padding-left: 16px;
-        display: flex;
-        align-items: center;
-        .name{
-            padding-right: 16px;
-        }
-        input{
-            height: 64px;
-            flex-grow: 1;
-            background: transparent;
-            border: none;
-            padding-right: 16px;
-        }
-    }
-    .tags {
-        border: 1px solid red;
-        font-size: 14px;
-        padding: 16px;
-        flex-grow: 1;
-        display: flex;
-        flex-direction: column-reverse;
-        >.current {
-            display: flex;
-            flex-wrap: wrap;
-            > li{
-                background: #d9d9d9;
-                $h:24px;
-                height: $h;
-                line-height: $h;
-                border-radius: $h/2;
-                padding: 0 16px;
-                margin-right: 12px;
-                margin-top: 4px;
-            }
-        }
-        > .new {
-            padding-top: 16px;
-            button{
-                background: transparent;
-                border: none;
-                color: #999;
-                border-bottom: 1px solid;
-                padding: 0 4px;
-            }
-        }
-    }
 
 </style>
