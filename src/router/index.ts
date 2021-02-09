8a0bec7e2a2c4d8505fe01b7b7f46acd9c8b5143
zdxzdxzdx <769371987@qq.com>
2021-02-09 13:56:17 +0800

    将tag的string改为{id,name}

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
-import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
 import NotFound from '@/views/NotFound.vue';
+import EditLabel from '@/views/EditLabel.vue';
 
 Vue.use(VueRouter);
 
@@ -25,6 +25,10 @@ const routes: Array<RouteConfig> = [
   {
     path:'/statistics',
     component:Statistics
+  },
+  {
+    path:'/labels/edit',
+    component:EditLabel
   },
   {
     path:'*',
---
+++ src/views/EditLabel.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        编辑标签
+    </div>
+</template>
+
+<script lang="ts">
+  import {Component} from 'vue-property-decorator';
+  import Vue from 'vue';
+
+  @Component
+  export default class EditLabel extends Vue {
+
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
